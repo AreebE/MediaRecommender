8b67694a8880ab78034d6ed098f8c2de3d4f4b65
pandenandini88 <102429378+pandenandini88@users.noreply.github.com>
2023-09-16 23:40:30 +0000

    Added content to music, all inputs are in, inputs still need to be organized, everything good rn

--- booksform.js
+++ booksform.js
@@ -1,23 +1,24 @@
-var genreslist =[];
+var genreslist = [];
 var ratings = [];
-function display()
-{
-//document.getElementById("displayarea").innerHTML = document.getElementById("genres").value;
-var genre = document.getElementById("genres").value;
-//console.log(genre);
-genreslist = genre.split(",");
-console.log(genreslist);
-var theme = document.getElementById("themes").value;
-//console.log(genre);
-themeslist = theme.split(",");
-console.log(themeslist);
-//document.getElementById("displayarea1").innerHTML = document.getElementById("authors").value; 
-var author = document.getElementById("authors").value;
-//console.log(author);
-const authorslist = author.split(",");
-console.log(authorslist);
-genresforcomb = genreslist;
-return genresforcomb;
+function display() {
+  //document.getElementById("displayarea").innerHTML = document.getElementById("genres").value;
+  var genre = document.getElementById("genres").value;
+  //console.log(genre);
+  genreslist = genre.split(",");
+  console.log(genreslist);
+  var theme = document.getElementById("themes").value;
+  themeslist = theme.split(",");
+  console.log(themeslist);
+  //document.getElementById("displayarea1").innerHTML = document.getElementById("authors").value; 
+  var author = document.getElementById("authors").value;
+  //console.log(author);
+  const authorslist = author.split(",");
+  console.log(authorslist);
+  var year = document.getElementById("years").value;
+  yearslist = year.split("-");
+  console.log(yearslist);
+  genresforcomb = genreslist;
+  return genresforcomb;
 }
 /*<tr style="background-color:#8FBC8F;">
 <td align="center"><b>Genres You Listed</b></td>
@@ -29,34 +30,34 @@ return genresforcomb;
 </tr>*/
 
 
-window.onload=function(){
-    document.getElementById("ratingForm").addEventListener("submit", function(event) {
-            event.preventDefault(); // Prevent the form from submitting
+window.onload = function() {
+  document.getElementById("ratingForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the form from submitting
 
-            // Collect and process the ratings
-            ratings = [];
+    // Collect and process the ratings
+    ratings = [];
 
-            // Loop through the form elements
-            var elements = this.elements;
-            for (var i = 0; i < elements.length; i++) {
-                var element = elements[i];
-                if (element.type === "number"  && element.name !== "numRec") {
-                    var rating = parseInt(element.value, 10);
+    // Loop through the form elements
+    var elements = this.elements;
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      if (element.type === "number" && element.name !== "numRec") {
+        var rating = parseInt(element.value, 10);
 
-                    // Check if the rating is within the valid range (1 to 10)
-                    if (rating >= 1 && rating <= 10) {
-                        ratings.push(rating);
-                    } else {
-                        //alert("Please enter a rating between 1 and 10.");
-                        return; // Exit the function if an invalid rating is found
-                    }
-                }
-            }
+        // Check if the rating is within the valid range (1 to 10)
+        if (rating >= 1 && rating <= 10) {
+          ratings.push(rating);
+        } else {
+          //alert("Please enter a rating between 1 and 10.");
+          return; // Exit the function if an invalid rating is found
+        }
+      }
+    }
 
-            // Display or further process the collected ratings here
-            console.log("User Ratings:", ratings);
-          console.log(ratings[1]);
-        });
+    // Display or further process the collected ratings here
+    console.log("User Ratings:", ratings);
+    console.log(ratings[1]);
+  });
 }
 
 
